d8d0116aa1db9443f21b860864d0577d3fd1529d
sultz8 <sultan.kurbanov98@gmail.com>
2023-02-02 19:41:10 +0500

    Обновил класс реквеста.

--- FuelLevelSensorRequest.php
+++ FuelLevelSensorRequest.php
@@ -25,19 +25,27 @@ class FuelLevelSensorRequest extends FormRequest
      */
     public function rules()
     {
+        $paramsRules = [
+            'params' => 'array|nullable',
+            'params.connection_port_name' => 'string|max:255',
+            'params.calibration_table' => 'array',
+            'params.calibration_table.*' => 'array|size:2',
+            'params.calibration_table.*.*' => 'numeric',
+        ];
+
         if ($this->isMethod('POST')) {
             return [
                 'model' => 'required|string|in:' . implode(',', FuelLevelSensor::MODELS),
                 'name' => 'required|string|max:255',
                 'tracker_id' => 'required|int|exists:trackers,id',
-                'params' => 'nullable|array',
+                ...$paramsRules
             ];
         } else {
             return [
                 'model' => 'string|in:' . implode(',', FuelLevelSensor::MODELS),
                 'name' => 'string|max:255',
                 'tracker_id' => 'int|exists:trackers,id',
-                'params' => 'nullable|array',
+                ...$paramsRules
             ];
         }
     }
